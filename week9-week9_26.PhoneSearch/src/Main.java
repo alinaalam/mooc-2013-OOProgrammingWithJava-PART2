634f2bdb3354f038dcc47c09ba4bd1b53017dd29
alinaalam <alinaalamzeb@gmail.com>
2019-04-09 23:32:59 +0200

    completed Week 9

--- week9-week9_26.PhoneSearch/src/Main.java
+++ week9-week9_26.PhoneSearch/src/Main.java
@@ -1,8 +1,13 @@
 
+import application.UI;
+
+
 public class Main {
 
     public static void main(String[] args) {
         // Start your program here
         // ATTENTION: In your program, you can create only one instance of class Scanner!
+        UI ui = new UI();
+        ui.start();
     }
 }
